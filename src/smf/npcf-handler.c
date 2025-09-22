3a7021b116ae4d737510e01f5232cd3b116aaa38
Jan Fiołka <35661939+JanF01@users.noreply.github.com>
2025-09-22 22:34:54 +0200

    Update npcf-handler.c
    fix

--- src/smf/npcf-handler.c
+++ src/smf/npcf-handler.c
@@ -706,14 +706,14 @@ bool smf_npcf_smpolicycontrol_handle_create(
     cp2up_pdr->precedence = OGS_PFCP_CP2UP_PDR_PRECEDENCE;
     up2cp_pdr->precedence = OGS_PFCP_UP2CP_PDR_PRECEDENCE;
 
-    void *pfcp_node_ptr = sess->pfcp_node;
+    smf_pfcp_node_t *pfcp_node_ptr = sess->pfcp_node;  // keep type for member access
     void *addr_list_ptr = pfcp_node_ptr ? pfcp_node_ptr->addr_list : NULL;
     void *gtpu_res_list_ptr = pfcp_node_ptr ? pfcp_node_ptr->gtpu_resource_list : NULL;
 
     ogs_info("[%s:%d] PFCP node=%p, pfcp_node->addr_list=%p, pfcp_node->gtpu_resource_list=%p",
              smf_ue->supi, sess->psi,
              pfcp_node_ptr, addr_list_ptr, gtpu_res_list_ptr);
-
+    
     ogs_assert(OGS_OK ==
             smf_5gc_pfcp_send_session_establishment_request(sess, stream, 0));
 
