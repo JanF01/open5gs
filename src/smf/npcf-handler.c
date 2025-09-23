78b7ffcc5b2ea171bca80068aeb36d95d5c41a26
Jan Fiołka <35661939+JanF01@users.noreply.github.com>
2025-09-23 12:35:08 +0200

    Update npcf-handler.c
    ff

--- src/smf/npcf-handler.c
+++ src/smf/npcf-handler.c
@@ -591,6 +591,11 @@ bool smf_npcf_smpolicycontrol_handle_create(
         smf_ue->supi, sess->session.name,
         sess->ipv4 ? OGS_INET_NTOP(&sess->ipv4->addr, buf1) : "",
         sess->ipv6 ? OGS_INET6_NTOP(&sess->ipv6->addr, buf2) : "");
+
+    const char *tmp_ipv4 = sess->ipv4 ? OGS_INET_NTOP(&sess->ipv4->addr, buf3) : "";
+    char *ipv4_copy = ogs_strdup(tmp_ipv4);
+    ogs_dbi_session_insert(smf_ue->supi, sess->session.name, ipv4_copy, NULL);
+    ogs_free(ipv4_copy);
     
     /* Set UPF N3 DL Outer-Header-Creation */
     if (sess->remote_dl_ip.ipv4 || sess->remote_dl_ip.ipv6) {
