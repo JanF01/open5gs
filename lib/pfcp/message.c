691d18f246621c405e0c75e3bd269c666dafd20b
JanF01 <jan.fiolka44@gmail.com>
2025-09-27 14:58:23 +0200

    [FIX] pfcp naming fix

--- lib/pfcp/message.c
+++ lib/pfcp/message.c
@@ -4704,8 +4704,8 @@ ogs_pfcp_message_t *ogs_pfcp_parse_msg(ogs_pkbuf_t *pkbuf)
 
     case OGS_PFCP_BLOCKCHAIN_CREDENTIALS_RESPONSE_TYPE:
         rv = ogs_tlv_parse_msg(
-            &pfcp_message->pfcp_blockchain_login_response,
-            &ogs_pfcp_msg_desc_pfcp_blockchain_login_response, pkbuf, OGS_TLV_MODE_T2_L2);
+            &pfcp_message->pfcp_blockchain_credentials_response,
+            &ogs_pfcp_msg_desc_pfcp_blockchain_credentials_response, pkbuf, OGS_TLV_MODE_T2_L2);
         ogs_expect(rv == OGS_OK);
         break;
     default:
@@ -4835,11 +4835,11 @@ ogs_pkbuf_t *ogs_pfcp_build_msg(ogs_pfcp_message_t *pfcp_message)
         pkbuf = ogs_tlv_build_msg(&ogs_pfcp_msg_desc_pfcp_session_report_response,
                                   &pfcp_message->pfcp_session_report_response, OGS_TLV_MODE_T2_L2);
         break;
-    case OGS_PFCP_BLOCKCHAIN_LOGIN_REQUEST_TYPE:
+    case OGS_PFCP_BLOCKCHAIN_CREDENTIALS_REQUEST_TYPE:
         pkbuf = ogs_tlv_build_msg(&ogs_pfcp_msg_desc_pfcp_blockchain_credentials_request,
                                   &pfcp_message->pfcp_blockchain_credentials_request, OGS_TLV_MODE_T2_L2);
         break;
-    case OGS_PFCP_BLOCKCHAIN_LOGIN_RESPONSE_TYPE:
+    case OGS_PFCP_BLOCKCHAIN_CREDENTIALS_RESPONSE_TYPE:
         pkbuf = ogs_tlv_build_msg(&ogs_pfcp_msg_desc_pfcp_blockchain_credentials_response,
                                   &pfcp_message->pfcp_blockchain_credentials_response, OGS_TLV_MODE_T2_L2);
         break;
