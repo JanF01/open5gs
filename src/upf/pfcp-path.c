da13624b9522561ec6994e1d3415d48b00018e7d
JanF01 <jan.fiolka44@gmail.com>
2025-09-27 15:29:51 +0200

    [FIX] placeholder return

--- src/upf/pfcp-path.c
+++ src/upf/pfcp-path.c
@@ -31,9 +31,10 @@ static void pfcp_node_fsm_init(ogs_pfcp_node_t *node, bool try_to_associate)
     memset(&e, 0, sizeof(e));
     e.pfcp_node = node;
 
-    if (try_to_associate == true) {
+    if (try_to_associate == true)
+    {
         node->t_association = ogs_timer_add(ogs_app()->timer_mgr,
-                upf_timer_association, node);
+                                            upf_timer_association, node);
         ogs_assert(node->t_association);
     }
 
@@ -65,13 +66,15 @@ static void pfcp_recv_cb(short when, ogs_socket_t fd, void *data)
     ogs_pfcp_node_t *node = NULL;
     ogs_pfcp_message_t *message = NULL;
 
-    ogs_pfcp_status_e pfcp_status;;
+    ogs_pfcp_status_e pfcp_status;
+    ;
     ogs_pfcp_node_id_t node_id;
 
     ogs_assert(fd != INVALID_SOCKET);
 
     pkbuf = ogs_pfcp_recvfrom(fd, &from);
-    if (!pkbuf) {
+    if (!pkbuf)
+    {
         ogs_error("ogs_pfcp_recvfrom() failed");
         return;
     }
@@ -86,7 +89,8 @@ static void pfcp_recv_cb(short when, ogs_socket_t fd, void *data)
      * it can cause stack overflow.
      * To avoid this, the pfcp_message structure uses heap memory.
      */
-    if ((message = ogs_pfcp_parse_msg(pkbuf)) == NULL) {
+    if ((message = ogs_pfcp_parse_msg(pkbuf)) == NULL)
+    {
         ogs_error("ogs_pfcp_parse_msg() failed");
         ogs_pkbuf_free(pkbuf);
         upf_event_free(e);
@@ -94,17 +98,16 @@ static void pfcp_recv_cb(short when, ogs_socket_t fd, void *data)
     }
 
     pfcp_status = ogs_pfcp_extract_node_id(message, &node_id);
-    switch (pfcp_status) {
+    switch (pfcp_status)
+    {
     case OGS_PFCP_STATUS_SUCCESS:
     case OGS_PFCP_STATUS_NODE_ID_NONE:
     case OGS_PFCP_STATUS_NODE_ID_OPTIONAL_ABSENT:
         ogs_debug("ogs_pfcp_extract_node_id() "
-                "type [%d] pfcp_status [%d] node_id [%s] from %s",
-                message->h.type, pfcp_status,
-                pfcp_status == OGS_PFCP_STATUS_SUCCESS ?
-                    ogs_pfcp_node_id_to_string_static(&node_id) :
-                    "NULL",
-                ogs_sockaddr_to_string_static(&from));
+                  "type [%d] pfcp_status [%d] node_id [%s] from %s",
+                  message->h.type, pfcp_status,
+                  pfcp_status == OGS_PFCP_STATUS_SUCCESS ? ogs_pfcp_node_id_to_string_static(&node_id) : "NULL",
+                  ogs_sockaddr_to_string_static(&from));
         break;
 
     case OGS_PFCP_ERROR_SEMANTIC_INCORRECT_MESSAGE:
@@ -112,54 +115,58 @@ static void pfcp_recv_cb(short when, ogs_socket_t fd, void *data)
     case OGS_PFCP_ERROR_NODE_ID_NOT_FOUND:
     case OGS_PFCP_ERROR_UNKNOWN_MESSAGE:
         ogs_error("ogs_pfcp_extract_node_id() failed "
-                "type [%d] pfcp_status [%d] from %s",
-                message->h.type, pfcp_status,
-                ogs_sockaddr_to_string_static(&from));
+                  "type [%d] pfcp_status [%d] from %s",
+                  message->h.type, pfcp_status,
+                  ogs_sockaddr_to_string_static(&from));
         goto cleanup;
 
     default:
         ogs_error("Unexpected pfcp_status "
-                "type [%d] pfcp_status [%d] from %s",
-                message->h.type, pfcp_status,
-                ogs_sockaddr_to_string_static(&from));
+                  "type [%d] pfcp_status [%d] from %s",
+                  message->h.type, pfcp_status,
+                  ogs_sockaddr_to_string_static(&from));
         goto cleanup;
     }
 
     node = ogs_pfcp_node_find(&ogs_pfcp_self()->pfcp_peer_list,
-            pfcp_status == OGS_PFCP_STATUS_SUCCESS ? &node_id : NULL, &from);
-    if (!node) {
+                              pfcp_status == OGS_PFCP_STATUS_SUCCESS ? &node_id : NULL, &from);
+    if (!node)
+    {
         if (message->h.type == OGS_PFCP_ASSOCIATION_SETUP_REQUEST_TYPE ||
-            message->h.type == OGS_PFCP_ASSOCIATION_SETUP_RESPONSE_TYPE) {
+            message->h.type == OGS_PFCP_ASSOCIATION_SETUP_RESPONSE_TYPE)
+        {
             ogs_assert(pfcp_status == OGS_PFCP_STATUS_SUCCESS);
             node = ogs_pfcp_node_add(&ogs_pfcp_self()->pfcp_peer_list,
-                    &node_id, &from);
-            if (!node) {
+                                     &node_id, &from);
+            if (!node)
+            {
                 ogs_error("No memory: ogs_pfcp_node_add() failed");
                 goto cleanup;
             }
             ogs_debug("Added PFCP-Node: addr_list %s",
-                    ogs_sockaddr_to_string_static(node->addr_list));
+                      ogs_sockaddr_to_string_static(node->addr_list));
 
             pfcp_node_fsm_init(node, false);
-
-        } else {
+        }
+        else
+        {
             ogs_error("Cannot find PFCP-Node: type [%d] node_id %s from %s",
-                    message->h.type,
-                    pfcp_status == OGS_PFCP_STATUS_SUCCESS ?
-                        ogs_pfcp_node_id_to_string_static(&node_id) :
-                        "NULL",
-                    ogs_sockaddr_to_string_static(&from));
+                      message->h.type,
+                      pfcp_status == OGS_PFCP_STATUS_SUCCESS ? ogs_pfcp_node_id_to_string_static(&node_id) : "NULL",
+                      ogs_sockaddr_to_string_static(&from));
             goto cleanup;
         }
-    } else {
+    }
+    else
+    {
         ogs_debug("Found PFCP-Node: addr_list %s",
-                ogs_sockaddr_to_string_static(node->addr_list));
+                  ogs_sockaddr_to_string_static(node->addr_list));
         ogs_expect(OGS_OK == ogs_pfcp_node_merge(
-                    node,
-                    pfcp_status == OGS_PFCP_STATUS_SUCCESS ?  &node_id : NULL,
-                    &from));
+                                 node,
+                                 pfcp_status == OGS_PFCP_STATUS_SUCCESS ? &node_id : NULL,
+                                 &from));
         ogs_debug("Merged PFCP-Node: addr_list %s",
-                ogs_sockaddr_to_string_static(node->addr_list));
+                  ogs_sockaddr_to_string_static(node->addr_list));
     }
 
     e->pfcp_node = node;
@@ -167,7 +174,8 @@ static void pfcp_recv_cb(short when, ogs_socket_t fd, void *data)
     e->pfcp_message = message;
 
     rv = ogs_queue_push(ogs_app()->queue, e);
-    if (rv != OGS_OK) {
+    if (rv != OGS_OK)
+    {
         ogs_error("ogs_queue_push() failed:%d", (int)rv);
         goto cleanup;
     }
@@ -186,20 +194,24 @@ int upf_pfcp_open(void)
     ogs_sock_t *sock = NULL;
 
     /* PFCP Server */
-    ogs_list_for_each(&ogs_pfcp_self()->pfcp_list, node) {
+    ogs_list_for_each(&ogs_pfcp_self()->pfcp_list, node)
+    {
         sock = ogs_pfcp_server(node);
-        if (!sock) return OGS_ERROR;
+        if (!sock)
+            return OGS_ERROR;
 
         node->poll = ogs_pollset_add(ogs_app()->pollset,
-                OGS_POLLIN, sock->fd, pfcp_recv_cb, sock);
+                                     OGS_POLLIN, sock->fd, pfcp_recv_cb, sock);
         ogs_assert(node->poll);
     }
-    ogs_list_for_each(&ogs_pfcp_self()->pfcp_list6, node) {
+    ogs_list_for_each(&ogs_pfcp_self()->pfcp_list6, node)
+    {
         sock = ogs_pfcp_server(node);
-        if (!sock) return OGS_ERROR;
+        if (!sock)
+            return OGS_ERROR;
 
         node->poll = ogs_pollset_add(ogs_app()->pollset,
-                OGS_POLLIN, sock->fd, pfcp_recv_cb, sock);
+                                     OGS_POLLIN, sock->fd, pfcp_recv_cb, sock);
         ogs_assert(node->poll);
     }
 
@@ -223,8 +235,8 @@ void upf_pfcp_close(void)
 }
 
 int upf_pfcp_send_session_establishment_response(
-        ogs_pfcp_xact_t *xact, upf_sess_t *sess,
-        ogs_pfcp_pdr_t *created_pdr[], int num_of_created_pdr)
+    ogs_pfcp_xact_t *xact, upf_sess_t *sess,
+    ogs_pfcp_pdr_t *created_pdr[], int num_of_created_pdr)
 {
     int rv;
     ogs_pkbuf_t *n4buf = NULL;
@@ -237,14 +249,16 @@ int upf_pfcp_send_session_establishment_response(
     h.seid = sess->smf_n4_f_seid.seid;
 
     n4buf = upf_n4_build_session_establishment_response(
-            h.type, sess, created_pdr, num_of_created_pdr);
-    if (!n4buf) {
+        h.type, sess, created_pdr, num_of_created_pdr);
+    if (!n4buf)
+    {
         ogs_error("upf_n4_build_session_establishment_response() failed");
         return OGS_ERROR;
     }
 
     rv = ogs_pfcp_xact_update_tx(xact, &h, n4buf);
-    if (rv != OGS_OK) {
+    if (rv != OGS_OK)
+    {
         ogs_error("ogs_pfcp_xact_update_tx() failed");
         return OGS_ERROR;
     }
@@ -256,8 +270,8 @@ int upf_pfcp_send_session_establishment_response(
 }
 
 int upf_pfcp_send_session_modification_response(
-        ogs_pfcp_xact_t *xact, upf_sess_t *sess,
-        ogs_pfcp_pdr_t *created_pdr[], int num_of_created_pdr)
+    ogs_pfcp_xact_t *xact, upf_sess_t *sess,
+    ogs_pfcp_pdr_t *created_pdr[], int num_of_created_pdr)
 {
     int rv;
     ogs_pkbuf_t *n4buf = NULL;
@@ -270,14 +284,16 @@ int upf_pfcp_send_session_modification_response(
     h.seid = sess->smf_n4_f_seid.seid;
 
     n4buf = upf_n4_build_session_modification_response(
-            h.type, sess, created_pdr, num_of_created_pdr);
-    if (!n4buf) {
+        h.type, sess, created_pdr, num_of_created_pdr);
+    if (!n4buf)
+    {
         ogs_error("upf_n4_build_session_modification_response() failed");
         return OGS_ERROR;
     }
 
     rv = ogs_pfcp_xact_update_tx(xact, &h, n4buf);
-    if (rv != OGS_OK) {
+    if (rv != OGS_OK)
+    {
         ogs_error("ogs_pfcp_xact_update_tx() failed");
         return OGS_ERROR;
     }
@@ -289,7 +305,7 @@ int upf_pfcp_send_session_modification_response(
 }
 
 int upf_pfcp_send_session_deletion_response(ogs_pfcp_xact_t *xact,
-        upf_sess_t *sess)
+                                            upf_sess_t *sess)
 {
     int rv;
     ogs_pkbuf_t *n4buf = NULL;
@@ -302,13 +318,15 @@ int upf_pfcp_send_session_deletion_response(ogs_pfcp_xact_t *xact,
     h.seid = sess->smf_n4_f_seid.seid;
 
     n4buf = upf_n4_build_session_deletion_response(h.type, sess);
-    if (!n4buf) {
+    if (!n4buf)
+    {
         ogs_error("upf_n4_build_session_deletion_response() failed");
         return OGS_ERROR;
     }
 
     rv = ogs_pfcp_xact_update_tx(xact, &h, n4buf);
-    if (rv != OGS_OK) {
+    if (rv != OGS_OK)
+    {
         ogs_error("ogs_pfcp_xact_update_tx() failed");
         return OGS_ERROR;
     }
@@ -333,12 +351,14 @@ static void sess_timeout(ogs_pfcp_xact_t *xact, void *data)
     ogs_assert(sess_id >= OGS_MIN_POOL_ID && sess_id <= OGS_MAX_POOL_ID);
 
     sess = upf_sess_find_by_id(sess_id);
-    if (!sess) {
+    if (!sess)
+    {
         ogs_error("Session has already been removed [%d]", type);
         return;
     }
 
-    switch (type) {
+    switch (type)
+    {
     case OGS_PFCP_SESSION_REPORT_REQUEST_TYPE:
         ogs_error("No PFCP session report response");
         break;
@@ -349,7 +369,7 @@ static void sess_timeout(ogs_pfcp_xact_t *xact, void *data)
 }
 
 int upf_pfcp_send_session_report_request(
-        upf_sess_t *sess, ogs_pfcp_user_plane_report_t *report)
+    upf_sess_t *sess, ogs_pfcp_user_plane_report_t *report)
 {
     int rv;
     ogs_pkbuf_t *n4buf = NULL;
@@ -366,24 +386,26 @@ int upf_pfcp_send_session_report_request(
     h.seid = sess->smf_n4_f_seid.seid;
 
     xact = ogs_pfcp_xact_local_create(
-            sess->pfcp_node, sess_timeout, OGS_UINT_TO_POINTER(sess->id));
-    if (!xact) {
+        sess->pfcp_node, sess_timeout, OGS_UINT_TO_POINTER(sess->id));
+    if (!xact)
+    {
         ogs_error("ogs_pfcp_xact_local_create() failed");
         return OGS_ERROR;
     }
 
     n4buf = ogs_pfcp_build_session_report_request(h.type, report);
-    if (!n4buf) {
+    if (!n4buf)
+    {
         ogs_error("ogs_pfcp_build_session_report_request() failed");
         return OGS_ERROR;
     }
 
     rv = ogs_pfcp_xact_update_tx(xact, &h, n4buf);
-    if (rv != OGS_OK) {
+    if (rv != OGS_OK)
+    {
         ogs_error("ogs_pfcp_xact_update_tx() failed");
         return OGS_ERROR;
     }
-
 
     rv = ogs_pfcp_xact_commit(xact);
     ogs_expect(rv == OGS_OK);
@@ -392,10 +414,10 @@ int upf_pfcp_send_session_report_request(
 }
 
 int upf_pfcp_blockchain_data_operation(
-        upf_sess_t *sess, ogs_pfcp_blockchain_data_t *blockchain)
+    upf_sess_t *sess, ogs_pfcp_blockchain_data_t *blockchain)
 {
     ogs_assert(sess);
-    //ogs_assert(pkbuf);
+    // ogs_assert(pkbuf);
 
     ogs_info("Performing blockchain data operation for session %p", sess);
     // TODO: Implement actual blockchain interaction here.
@@ -403,7 +425,8 @@ int upf_pfcp_blockchain_data_operation(
     // interacting with a blockchain client, and sending a response.
 
     // For now, just log the packet data
-  //  ogs_log_hexdump(OGS_LOG_INFO, pkbuf->data, pkbuf->len);
+    //  ogs_log_hexdump(OGS_LOG_INFO, pkbuf->data, pkbuf->len);
 
-   // return OGS_OK;
+    // return OGS_OK;
+    return 1;
 }
