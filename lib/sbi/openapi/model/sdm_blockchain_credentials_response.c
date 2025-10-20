71556c17fa961dfd8cdd3e54a811eedbe9d5fe55
JanF01 <jan.fiolka44@gmail.com>
2025-10-20 15:53:59 +0200

    [FIX] fixed credentials_response

--- lib/sbi/openapi/model/sdm_blockchain_credentials_response.c
+++ lib/sbi/openapi/model/sdm_blockchain_credentials_response.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include "../include/ogs_assert.h"
+#include "sdm_blockchain_node_id.h"
 
-OpenAPI_sdm_blockchain_credentials_response_t *OpenAPI_sdm_blockchain_credentials_response_create(OpenAPI_sdm_blockchain_node_id_t *node_id)
+// Create
+OpenAPI_sdm_blockchain_credentials_response_t *
+OpenAPI_sdm_blockchain_credentials_response_create(OpenAPI_sdm_blockchain_node_id_t *node_id)
 {
     OpenAPI_sdm_blockchain_credentials_response_t *obj = ogs_malloc(sizeof(OpenAPI_sdm_blockchain_credentials_response_t));
     ogs_assert(obj);
@@ -12,6 +14,7 @@ OpenAPI_sdm_blockchain_credentials_response_t *OpenAPI_sdm_blockchain_credential
     return obj;
 }
 
+// Free
 void OpenAPI_sdm_blockchain_credentials_response_free(OpenAPI_sdm_blockchain_credentials_response_t *obj)
 {
     if (!obj)
@@ -24,11 +27,16 @@ void OpenAPI_sdm_blockchain_credentials_response_free(OpenAPI_sdm_blockchain_cre
     ogs_free(obj);
 }
 
+// Convert to JSON
 cJSON *OpenAPI_sdm_blockchain_credentials_response_convertToJSON(OpenAPI_sdm_blockchain_credentials_response_t *obj)
 {
     if (!obj)
         return NULL;
+
     cJSON *item = cJSON_CreateObject();
+    if (!item)
+        return NULL;
+
     if (obj->node_id)
     {
         cJSON *node_id_JSON = OpenAPI_sdm_blockchain_node_id_convertToJSON(obj->node_id);
@@ -40,15 +48,20 @@ cJSON *OpenAPI_sdm_blockchain_credentials_response_convertToJSON(OpenAPI_sdm_blo
         }
         cJSON_AddItemToObject(item, "nodeId", node_id_JSON);
     }
+
     return item;
 }
 
-OpenAPI_sdm_blockchain_credentials_response_t *OpenAPI_sdm_blockchain_credentials_response_parseFromJSON(cJSON *json)
+// Parse from JSON
+OpenAPI_sdm_blockchain_credentials_response_t *
+OpenAPI_sdm_blockchain_credentials_response_parseFromJSON(cJSON *json)
 {
     if (!json)
         return NULL;
+
     cJSON *node_id_json = cJSON_GetObjectItemCaseSensitive(json, "nodeId");
     OpenAPI_sdm_blockchain_node_id_t *node_id_obj = NULL;
+
     if (node_id_json)
     {
         node_id_obj = OpenAPI_sdm_blockchain_node_id_parseFromJSON(node_id_json);
@@ -58,5 +71,26 @@ OpenAPI_sdm_blockchain_credentials_response_t *OpenAPI_sdm_blockchain_credential
             return NULL;
         }
     }
+
     return OpenAPI_sdm_blockchain_credentials_response_create(node_id_obj);
+}
+
+// Deep copy
+OpenAPI_sdm_blockchain_credentials_response_t *
+OpenAPI_sdm_blockchain_credentials_response_copy(OpenAPI_sdm_blockchain_credentials_response_t *dst, OpenAPI_sdm_blockchain_credentials_response_t *src)
+{
+    if (!src)
+        return NULL;
+    if (!dst)
+        return OpenAPI_sdm_blockchain_credentials_response_create(
+            OpenAPI_sdm_blockchain_node_id_copy(NULL, src->node_id));
+
+    if (dst->node_id)
+    {
+        OpenAPI_sdm_blockchain_node_id_free(dst->node_id);
+        dst->node_id = NULL;
+    }
+
+    dst->node_id = OpenAPI_sdm_blockchain_node_id_copy(NULL, src->node_id);
+    return dst;
 }
