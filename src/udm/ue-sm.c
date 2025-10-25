5e4d012c6d64e5591f127aae75e69504085a64d2
JanF01 <jan.fiolka44@gmail.com>
2025-10-25 11:31:13 +0200

    [FIX] Fix to handling UDR reponse in UDM, resource type mismatch

--- src/udm/ue-sm.c
+++ src/udm/ue-sm.c
@@ -333,7 +333,7 @@ void udm_ue_state_operational(ogs_fsm_t *s, udm_event_t *e)
                     END
                 END
                 break;
-            CASE(OGS_SBI_RESOURCE_NAME_SDM_BLOCKCHAIN_NODE_ID)
+            CASE(OGS_SBI_RESOURCE_NAME_SDM_BLOCKCHAIN_CREDENTIALS)
                if(udm_nudm_sdm_handle_blockchain_node_id(udm_ue, stream, message)){
                         ogs_warn("udm_nudr_dr_handle_subscription_"
                                 "authentication() failed");
